f3eef7ead95742bddf970b3f034ab4798698dac0
Stephen Sandlin <info@SandlinProds.com>
2020-06-27 12:49:35 -0400

    Walk Animation Added

--- Fantasy Frontier/Assets/Scripts/ThirdPersonMovement.cs
+++ Fantasy Frontier/Assets/Scripts/ThirdPersonMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;//reference to the CharacterController on our player
+    public Animator animator;
     public Transform cam;//use main camera and not the Cinamachine camera
     public Transform groundCheck;//this is our groundcheck gameobject on the player
     public float speed = 6f;//speed of the Character
@@ -30,13 +31,46 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f) //this is where the magic happens with moving the player and making it so the player's forward is the way the camera is facing
         {
+            
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //this rotates our player
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);//this makes a smooth rotation of our player
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;//this makes it so we move forward in the direction of the camera
             controller.Move(moveDir.normalized * speed * Time.deltaTime);//this is what actully moves our player (i think)
+            var animationSpeedMultiplier = SetCorrectAnimation();
+            velocity *= animationSpeedMultiplier;
+        }
+        else
+        {
+            animator.SetFloat("move", 0);
         }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+    private float SetCorrectAnimation()
+    {
+        float currentAnimationSpeed = animator.GetFloat("move");
+        if(turnSmoothVelocity > 10 || turnSmoothVelocity < -10)
+        {
+            if(currentAnimationSpeed <0.2f)
+            {
+                currentAnimationSpeed += Time.deltaTime *2;
+                currentAnimationSpeed = Mathf.Clamp(currentAnimationSpeed, 0, .2f);
+            }
+            animator.SetFloat("move", currentAnimationSpeed);
+        }
+        else
+        {
+            if(currentAnimationSpeed<1)
+            {
+                currentAnimationSpeed += Time.deltaTime *2;
+            }
+            else
+            {
+                currentAnimationSpeed = 1;
+            }
+            animator.SetFloat("move", currentAnimationSpeed);
+        }
+        return currentAnimationSpeed;
     }
 }
